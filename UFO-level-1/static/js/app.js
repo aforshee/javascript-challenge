30fe155ad5eacaba8dd8d3a91600406511dbc09a
aforshee <35928602+aforshee@users.noreply.github.com>
2020-05-25 11:49:40 -0400

    final

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -26,9 +26,12 @@ var form = d3.select("#form");
 // Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
-form.on("submit", addDataToTbody);
+
 // Complete the event handler function for the form
 function runEnter() {
+
+    // clear html table
+    document.getElementById("output").innerHTML = "";
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
@@ -43,12 +46,8 @@ function runEnter() {
   
     // Create filtered data variable
     var filteredData = sightings.filter(sighting => sighting.datetime === inputValue);
-  
     console.log(filteredData);
   
-    // clear html table
-    document.getElementById("ufo-table").innerHTML = "";
-    
     //Replace ufo-table data with filtered data
     addDataToTbody(ufotableTbody, filteredData);
 
